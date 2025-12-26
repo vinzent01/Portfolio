06aa1faed3e85cc0d74e6fe216ca026f4176b617
Vinzent <vinzent0101@gmail.com>
2025-12-26 00:09:20 -0300

    added new contact

--- src/components/ContactList.tsx
+++ src/components/ContactList.tsx
@@ -9,6 +9,10 @@ export default function ContactList(){
         <img src="/svg/github.svg" width={52} height={52}></img>
         <a href="https://github.com/vinzent01" target='_blank'>github.com/vinzent01</a>
       </div>
+      <div className="contact-item">
+        <img src="/svg/phone.svg"></img>
+        <a href="tel:+5511994336946">+55 11 994336946</a>
+      </div>
     </div>
   )
 }
